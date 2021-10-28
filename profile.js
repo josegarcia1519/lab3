881a0aaa7164d838d6e9a74fcb7f5e01d11295fd
josegarcia1519 <josegarcia1519@gmail.com>
2021-10-28 01:52:23 -0600

    update

--- profile.js
+++ profile.js
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang = 'en'>
+<head>
+  <meta charset="UTF-8">
+  <title>JavaScript Events </title>
+  link rel="stylesheet" href="style.css">
+</head>
 <body>
 
 <h2>JavaScript addEventListener()</h2>
-
-<p>This example uses the addEventListener() method to attach a click event to a button.</p>
-
-<button id="myBtn">Try it</button>
-
-<p id="demo"></p>
-
 <script>
-document.getElementById("myBtn").addEventListener("click", displayDate);
-
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-
 document.getElementById('color').addEventListener('click', function(){
-  alert('my favorite color is red');
-}, false);
-document.getElementById('place').addEventListener('click', function(){
-  alert('my favorite place is lehi');
-}, false);
-document.getElementById('ritual').addEventListener('click', function(){
-  alert('my favorite ritual is yoga');
-}, false);
+  alert('my favorite color is red')
+  }
 
-<script src="profile.js"></script>
+document.getElementById('place').addEventListener('click', function(){
+  alert('my favorite place is lehi')
+  }
+  
+document.getElementById('ritual').addEventListener('click', function(){
+  alert('my favorite ritual is yoga')
+}
+</script>
+
+
 </body>
 </html>
